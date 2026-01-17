010d8397e4d7d2007c145b5c888a871ff031292f
Venclise <zunnurainzahoor955@gmail.com>
2026-01-18 01:50:05 +0500

    Changed

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,33 +1,33 @@
-import { NextRequest, NextResponse } from "next/server";
-import { connectDB } from "../../lib/db";
-import { Order } from "../../models/order";
+// import { NextRequest, NextResponse } from "next/server";
+// import { connectDB } from "../../lib/db";
+// import { Order } from "../../models/order";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
-export async function GET(
-  request: NextRequest,
-  context: any 
-) {
-  try {
-    await connectDB();
+// export async function GET(
+//   request: NextRequest,
+//   context: any 
+// ) {
+//   try {
+//     await connectDB();
 
-    const { id } =  context.params;
+//     const { id } =  context.params;
 
-    const order =  Order.findById(id);
+//     const order =  Order.findById(id);
 
-    if (!order) {
-      return NextResponse.json(
-        { message: "Order not found" },
-        { status: 404 }
-      );
-    }
+//     if (!order) {
+//       return NextResponse.json(
+//         { message: "Order not found" },
+//         { status: 404 }
+//       );
+//     }
 
-    return NextResponse.json(order);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Failed to fetch order" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(order);
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json(
+//       { message: "Failed to fetch order" },
+//       { status: 500 }
+//     );
+//   }
+// }
