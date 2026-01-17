17583cb5e7b93c0429ae4ddb5c8f5c5970542698
Zunnurian <139066323+Venclise@users.noreply.github.com>
2026-01-17 23:36:00 +0500

    Update route.ts

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,19 +1,17 @@
-// @ts-nocheck
-
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { connectDB } from "../../lib/db";
 import { Order } from "../../models/order";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(
-  request: NextRequest,
-  context: any
+  _req: Request,
+  { params }: { params: { id: string } }
 ) {
   try {
     await connectDB();
 
-    const { id } = await context.params;
-
-    const order = await Order.findById(id);
+    const order = await Order.findById(params.id);
 
     if (!order) {
       return NextResponse.json(
