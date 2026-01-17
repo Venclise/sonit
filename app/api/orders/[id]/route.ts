92c64580cecd3d44041ac91ac1250062f621f9b8
Zunnurian <139066323+Venclise@users.noreply.github.com>
2026-01-17 23:26:59 +0500

    Update route.ts

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,15 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { connectDB } from "../../lib/db";
 import { Order } from "../../models/order";
 
-export async function GET(
-  request: NextRequest,
-  context: any 
-) {
+export const dynamic = "force-dynamic";
+
+export async function GET(_req: Request, { params }: any) {
   try {
     await connectDB();
 
-    const { id } = await context.params;
+    const id = params.id;
 
     const order = await Order.findById(id);
 
@@ -22,7 +21,7 @@ export async function GET(
 
     return NextResponse.json(order);
   } catch (error) {
-    console.error(error);
+    console.error("Order fetch error:", error);
     return NextResponse.json(
       { message: "Failed to fetch order" },
       { status: 500 }
