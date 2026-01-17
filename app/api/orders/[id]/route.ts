08ef8745fca03750375dcc476e919a21cfdb3cc6
Venclise <zunnurainzahoor955@gmail.com>
2026-01-17 23:59:23 +0500

    I hate TypeScript

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -4,17 +4,13 @@ import { Order } from "../../models/order";
 
 export const dynamic = "force-dynamic";
 
-
-
 export async function GET(req: NextRequest, context: any) {
   try {
     await connectDB();
 
-    const id = context.params.id; 
-   
+    const id = context.params.id;
 
     const order = await Order.findById(id);
-    
 
     if (!order) {
       return NextResponse.json(
@@ -25,7 +21,6 @@ export async function GET(req: NextRequest, context: any) {
 
     return NextResponse.json(order);
   } catch (error) {
-    console.error(error);
     console.error(error);
     return NextResponse.json(
       { message: "Failed to fetch order" },
