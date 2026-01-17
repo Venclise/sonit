66055f9139ffa637ceda236cb360e045b435af6d
Venclise <zunnurainzahoor955@gmail.com>
2026-01-18 02:05:00 +0500

    i hate ts

---
+++ app/api/orders/[id]/route.js
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectDB } from "../../lib/db";
+import { Order } from "../../models/order";
+
+
+
+export async function GET(
+  request,
+  context
+) {
+  try {
+    await connectDB();
+
+    const { id } =  context.params;
+
+    const order =  Order.findById(id);
+
+    if (!order) {
+      return NextResponse.json(
+        { message: "Order not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(order);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Failed to fetch order" },
+      { status: 500 }
+    );
+  }
+}
